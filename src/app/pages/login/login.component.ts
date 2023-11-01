2a5142f77ea95f8657570196b5ed6f4201283506
muhammadfandu <muhammad.p.widodo@gmail.com>
2023-11-02 05:23:14 +0700

    feat: adjust login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +7,17 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
   ngOnDestroy() {
+  }
+
+  onLoginClick() {
+    this.router.navigateByUrl('/dashboard')
   }
 
 }
